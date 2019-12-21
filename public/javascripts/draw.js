46a8e1f4f1d7bbbc07835d91e5ff0717284a3c5d
RachelCT1 <1064607513@qq.com>
2019-12-21 09:58:32 +0800

    加入了Echarts;新建了一条路由，对应first.html和firstWeek.js用于查出data2中第一周的数据。这里一周的数据只保留了所有的食堂和便利店，不考虑其他地点的消费。

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -2,10 +2,11 @@ $.ajax({
     type:"get",
     url:"/query",
     data:{
-        sql:"select * from data1"
+        sql:"select *from data2 where  STR_TO_DATE(Date,'%Y/%m/%d %H:%i') BETWEEN '2019/4/1 6:00' and '2019/4/1 7:00'"
     },
     dataType:"json",
     success:function(data){
-        console.log(data)
+        // console.log(data);
+        return data;
     }
 })
